f943ca7dc46c4c9f3282010c19450222ebf4af69
Aron Hegyi <ahegyi@gmail.com>
2015-06-15 03:38:28 -0700

    Timers working-ish
    add tock

--- public/js/js.js
+++ public/js/js.js
@@ -1,3 +1,44 @@
+var ProgressTimer;
+
+ProgressTimer = (function() {
+  var progressHtml;
+
+  progressHtml = '<div class="progress-timer" style="display: none; margin-top: 10px;">' + '<p class="timing badge"></p>' + '<div class="progress">' + '<div class="progress-bar" role="progressbar" style="min-width: 5%;">' + '</div></div></div>';
+
+  function ProgressTimer(timerHighlight) {
+    var milliSeconds, progressTimer;
+    this.timerHighlight = timerHighlight;
+    progressTimer = $(progressHtml);
+    progressTimer.find('.timing').text(this.timerHighlight.text());
+    progressTimer.appendTo(this.timerHighlight.closest('.step'));
+    progressTimer.show();
+    this.minutes = parseInt(this.timerHighlight.data('minutes'), 10);
+    milliSeconds = this.minutes * 60 * 1000;
+    this.timer = new Tock({
+      countdown: true,
+      interval: 1000,
+      callback: function() {
+        var currentTime, newWidth;
+        currentTime = this.msToTime(this.lap());
+        progressTimer.find('.progress-bar').text(currentTime);
+        newWidth = (milliSeconds - this.lap()) / milliSeconds;
+        progressTimer.find('.progress-bar').css('width', newWidth * 100 + '%');
+      },
+      complete: function() {
+        return console.log('timer is done');
+      }
+    });
+  }
+
+  ProgressTimer.prototype.start = function() {
+    return this.timer.start(this.minutes + ':00');
+  };
+
+  return ProgressTimer;
+
+})();
+
+
 $(document).ready(function() {
   var steps = $(".step");
   var btnEl = $("button.start-cooking");
@@ -31,6 +72,17 @@ $(document).ready(function() {
     checkButtonText(steps, currentStep, btnEl);
     steps.removeClass("current");
     resetEl.hide();
+  });
+
+  steps.find(".timer").one("click", function(evt){
+    // get out of this function if not the current step
+    // i.e. don't activate timer unless current
+    if (!$(this).closest(".step").hasClass("current")) {
+      return true;
+    }
+
+    var p = new ProgressTimer($(this));
+    p.start();
   });
 });
 
