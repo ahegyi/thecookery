fc9b60de1b3f5635a9b14efd6496a00d8fb7e5d5
Aron Hegyi <ahegyi@gmail.com>
2015-06-15 02:02:06 -0700

    Update the js; highlight timings

--- public/js/js.js
+++ public/js/js.js
@@ -1,13 +1,42 @@
 $(document).ready(function() {
   var steps = $(".step");
+  var btnEl = $(".start-cooking");
+  var currentStep = -1;
 
   steps.on("click", function() {
-    if ($(this).hasClass("current")) {
-      $(this).removeClass("current");
-    }
-    else {
-      steps.removeClass("current");
-      $(this).addClass("current");
+    currentStep = steps.index(this);
+    toggleStep(steps, currentStep);
+    checkButtonText(steps, currentStep, btnEl);
+  });
+
+  btnEl.on("click", function() {
+    currentStep += 1;
+
+    toggleStep(steps, currentStep);
+    checkButtonText(steps, currentStep, btnEl);
+
+    if (currentStep >= steps.length) {
+      currentStep = -1;
     }
   });
 });
+
+var toggleStep = function(steps, currentStep) {
+  steps.removeClass("current");
+
+  if (currentStep !== -1) {
+    steps.eq(currentStep).addClass("current");
+  }
+};
+
+var checkButtonText = function(steps, currentStep, btnEl) {
+  if (currentStep === steps.length - 1) {
+    btnEl.text("Done!");
+  }
+  else if (currentStep === -1 || currentStep === steps.length) {
+    btnEl.text("Let's Cook!");
+  }
+  else {
+    btnEl.text("Next step...");
+  }
+};
--- web.rb
+++ web.rb
@@ -11,6 +11,19 @@ end
 
 get "/recipes/:id" do
   @recipe = Recipe.new(params["id"])
+  recipe_steps = @recipe.action_steps
+
+  # either N minutes, N-M minutes, or N to M minutes
+  # or maybe N mins or M minute
+  minute_matcher = /(\d+|\d+\D{1,4}\d+)\s+min(ute)?s*/
+
+  @recipe_steps = recipe_steps.map do |step|
+    step[1].gsub!(
+      minute_matcher,
+      '<strong class="timer" data-minutes="\1">\0</strong>'
+    )
+    step
+  end
 
   haml :recipe, format: :html5
 end
